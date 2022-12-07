e22df19a84e18abdb0faabb4112ca1f1b519b8b9
Oumar <baarryoumar@gmail.com>
2022-12-07 00:10:35 -0500

    First connection

--- demo/src/main/java/com/project4/classTest.java
+++ demo/src/main/java/com/project4/classTest.java
@@ -1,36 +1,31 @@
 package com.project4;
-//import java.util.Random;
 
+import java.sql.Statement;
 import java.io.IOException;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
-//import javax.swing.JOptionPane;
 
-//import javax.swing.JOptionPane;
 
 public class classTest {
-    public static void main(String[] args) throws IOException {
-      
-        MyPoint p = new MyPoint(100, 100, MyColor.RED);
-        String filename = "Project1/War_and_Peace.txt";
-        //String filename = JOptionPane.showInputDialog("Enter the name of the file:");
-        HistogramAlphaBet histogram = new HistogramAlphaBet(filename);
-        Map<Character, Integer> example = histogram.sortDownFrequency();
-        example.forEach((K, V) -> System.out.println(K + ": " + V));
-        System.out.println("\nNumber of characters: " + example.values().stream().reduce(0, (x, y) -> x + y) + "\n");
-        example = histogram.sortDownFrequency();
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306/world";
+        String username = "root";
+        String password = "Jkjkjk94+";
+        String query = "SELECT Name FROM city WHERE ID=1";
 
-        HistogramAlphaBet.MyPieChart pie = histogram.new MyPieChart(6, p, 50, 30);
-        System.out.println("Probability : " + histogram.getProbability());
-        System.out.println("\nFrequencies: " + example);
-        System.out.println("\nSum of prob: " + histogram.getSumOfProbability()+"\n");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement statement = connection.createStatement();
+        ResultSet set = statement.executeQuery(query);
+        
+        set.next();
+        String name = set.getString("Name");
+        System.out.println(name);
 
-        Map<Character, Slice> slices = pie.getMyPieChart();
-        slices.forEach((K,V) -> System.out.println(K + ": " + slices.get(K)));
-
-        double sumOfAngles = 0.0;
-        for (Character key : slices.keySet()){sumOfAngles += slices.get(key).getRotationAngle();}
-        System.out.println("Sum: " + sumOfAngles);
-
+        statement.close();
+        connection.close();
     }
 } //end of classTest
